28ffc88baf07e2228b6d05eea4d5b9593c0e3828
stijnisloll <47426618+Stijn-Kamp@users.noreply.github.com>
2021-12-09 15:00:38 +0000

    Added new features and cleaned up code Jan added a function to give tips on champion counters Build messages are now properly formatted

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-#import discord #import all the necessary modules
+import discord #import all the necessary modules
 from discord.ext import commands
 import os
 import random
+
+# for champion_counter
+from bs4 import BeautifulSoup
+import requests
 
 from Constants import MYTHICS, CHAMPIONS, BOOTS, ITEMS, ROLES, SUMMONER_SPELLS, RUNES, QUOTES
 
@@ -15,6 +19,19 @@ bot = commands.Bot(
   help_command= help_command
   ) #define command decorator
 
+# Functions
+def champion_counter(champion):
+  BASE_URL = "https://lolcounter.com/champions/{}"
+  URL = BASE_URL.format(champion)
+  try:
+    page = requests.get(URL).text
+    soup = BeautifulSoup(page, 'html.parser')
+    tips = soup.find_all("span", class_="_tip")
+    tip = random.choice(tips).text
+  except Exception as e:
+    print(e)
+    tip = "Sorry, I couldn't find a tip to play against {}".format(champion)
+  return tip
 
 # random functions
 def random_quote():
@@ -116,63 +133,112 @@ class CommandsHandler(commands.Cog):
       elif isinstance(error, commands.UserInputError):
           message = "Something about your input was wrong, please check your input and try again!"
       else:
+          print(error)
           message = "Oh no! Something went wrong while running the command!"
 
       await ctx.send(message, delete_after=5)
       await ctx.message.delete(delay=5)
 
   @commands.command()
-  async def late(ctx):
+  async def late(self, ctx):
     "Calculates if you have early or late game"
     await ctx.send(late_game())
 
   @commands.command()
-  async def quote(ctx):
+  async def counter(self, ctx, champion):
+    "Gives a random tip to counter a champion"
+    await ctx.send(champion_counter(champion))
+
+
+  @commands.command()
+  async def quote(self, ctx):
     "Displays a random champion quote"
     await ctx.send(random_quote())
 
   @commands.command()
-  async def mythic(ctx):
+  async def mythic(self, ctx):
     "Displays one random mythic"
     await ctx.send(random_mythic())
 
   @commands.command()
-  async def item(ctx):
+  async def item(self, ctx):
     "Displays one random unique item"
     await ctx.send(random_item())
 
   @commands.command()
-  async def boots(ctx):
+  async def boots(self, ctx):
     "Displays a random pair of boots"
     await ctx.send(random_boots())
 
   @commands.command()
-  async def champion(ctx):
+  async def champion(self, ctx):
     "Displays a random champion"
     await ctx.send(random_champion())
 
   @commands.command()
-  async def role(ctx):
+  async def role(self, ctx):
     "Displays a random role"
     await ctx.send(random_role())
 
   @commands.command()
-  async def runes(ctx):
+  async def runes(self, ctx):
     "Displays a runepage with random keystones"
     await ctx.send(random_runes())
 
 
   @commands.command(name="sum")
-  async def summoner_spell(ctx):
+  async def summoner_spell(self, ctx):
     "Displays a random summoners spell"
     await ctx.send(random_summoner_spell())
 
+
   @commands.command()
-  async def build(ctx):
+  async def build(self, ctx):
+    # https://python.plainenglish.io/python-discord-bots-formatting-text-efca0c5dc64a
+    # formatting example
     "Generates a random build"
+    PATCH = '11.24.1'
     build = random_build()
-    build = '\n'.join(["{}: {}".format(*item) for item in build.items()])
-    await ctx.send(build)
+    champion = build.get('Champion')
+    primary = build.get('Primary runes')
+    secondary = build.get('Secondary runes')
+    sums = ', '.join(build.get('Summoner spells'))
+    items = ', '.join(build.get('Items'))
+
+    embed=discord.Embed(color=discord.Color.blue())
+    embed.set_author(
+      name="{}".format(champion), icon_url="http://ddragon.leagueoflegends.com/cdn/{}/img/champion/{}.png".format(PATCH, champion)
+    )
+
+    embed.add_field(name="**Role**", value=build.get('Role'), inline=False)
+    embed.add_field(name="**Primary runes - {}**".format(primary.get('name')), value=', '.join(primary.get('keystones')), inline=False)
+    embed.add_field(name="**Secondary runes - {}**".format(secondary.get('name')), value=', '.join(secondary.get('keystones')), inline=False)
+    embed.add_field(name="**Summoner spells**", value=sums, inline=False)
+    embed.add_field(name="**Boots**", value=build.get('Boots'), inline=False)
+    embed.add_field(name="**Mythic**", value=build.get('Mythic'), inline=False)
+    embed.add_field(name="**Items**", value=items, inline=False)
+
+    await ctx.send(embed=embed)
+
+  @commands.command()
+  async def embed(self, ctx):
+      embed=discord.Embed(
+      title="Text Formatting",
+          url="https://realdrewdata.medium.com/",
+          description="Here are some ways to format text",
+          color=discord.Color.blue())
+      embed.set_author(name="RealDrewData", url="https://twitter.com/RealDrewData", icon_url="https://cdn-images-1.medium.com/fit/c/32/32/1*QVYjh50XJuOLQBeH_RZoGw.jpeg")
+      #embed.set_author(name=ctx.author.display_name, url="https://twitter.com/RealDrewData", icon_url=ctx.author.avatar_url)
+      embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
+      embed.add_field(name="*Italics*", value="Surround your text in asterisks (\*)", inline=False)
+      embed.add_field(name="**Bold**", value="Surround your text in double asterisks (\*\*)", inline=False)
+      embed.add_field(name="__Underline__", value="Surround your text in double underscores (\_\_)", inline=False)
+      embed.add_field(name="~~Strikethrough~~", value="Surround your text in double tildes (\~\~)", inline=False)
+      embed.add_field(name="`Code Chunks`", value="Surround your text in backticks (\`)", inline=False)
+      embed.add_field(name="Blockquotes", value="> Start your text with a greater than symbol (\>)", inline=False)
+      embed.add_field(name="Secrets", value="||Surround your text with double pipes (\|\|)||", inline=False)
+      embed.set_footer(text="Learn more here: realdrewdata.medium.com")
+      await ctx.send(embed=embed)
 
 def setup(bot: commands.Bot):
   bot.add_cog(CommandsHandler(bot))
@@ -201,7 +267,6 @@ async def on_message(message):
     
     await bot.process_commands(message)
 
-
-
 setup(bot)
 bot.run(DISCORD_TOKEN) #run the client using using my bot's token
+
