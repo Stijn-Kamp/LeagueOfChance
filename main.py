39d701ce00412d69a3ea0c22450d9e01a5167f28
Stijn Kamp <stijnkamp@hotmail.com>
2022-03-31 10:53:46 +0200

    refractured main.py added error handling

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import os
 from dotenv import load_dotenv
 from Cogs.Bot import DiscordBot
+from discord import LoginFailure
+
+def get_token():
+  """
+  Loads discord token
+  """
+  TOKEN_NAME = "TOKEN"
+
+  load_dotenv()
+  try:
+    os.environ[TOKEN_NAME]
+    token = os.environ[TOKEN_NAME]
+  except KeyError:
+    token = None
+  return token
+
 
 if __name__ == '__main__':
-  load_dotenv()
-  os.environ["TOKEN"]
-  DISCORD_TOKEN = os.environ["TOKEN"]
+  DISCORD_TOKEN = get_token()
+  if DISCORD_TOKEN is None:
+    print("Please make sure that the token is stored in the right place.")
+    exit(0)
 
+try:
   DiscordBot(DISCORD_TOKEN)
+except LoginFailure:
+  print("Improper token has been passed.")
+finally:
+  exit(0)
